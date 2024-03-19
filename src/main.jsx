cb0cd0304f1375653ce03af189cb10e86a3bb874
sadekcric4 <sadekcric4@gmail.com>
2024-03-19 19:29:14 +0600

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+  },
+  {
+    path: "/about",
+    element: <div>Now I am in About Page</div>,
+  },
+  {
+    path: "/contract",
+    element: <div> This is Contract Page</div>,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
